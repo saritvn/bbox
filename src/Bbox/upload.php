79731ed0930eabe4f789c03178b4d947e18fa693
Ha Trung <saritvn@gmail.com>
2017-05-09 09:08:02 +0700

    -- Test Composer

---
+++ src/Bbox/upload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bbox;
+
+/**
+ * This generator returns a default value for all called properties
+ * and methods. It works with Faker\Generator\Base->optional().
+ */
+class Upload
+{
+    protected $default;
+    public function test();
+}
